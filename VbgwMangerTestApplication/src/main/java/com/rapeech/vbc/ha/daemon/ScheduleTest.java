8f9cc0c8f1cdc74f0b019da459a7cc5e34c48e8a
RapeechDawoon <itdawoonkim@gmail.com>
2024-02-14 17:09:17 +0900

    Add for testing API(GET)

--- VbgwMangerTestApplication/src/main/java/com/rapeech/vbc/ha/daemon/ScheduleTest.java
+++ VbgwMangerTestApplication/src/main/java/com/rapeech/vbc/ha/daemon/ScheduleTest.java
@@ -3,6 +3,7 @@ package com.rapeech.vbc.ha.daemon;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @EnableScheduling
@@ -10,6 +11,14 @@ public class ScheduleTest {
 
     @Scheduled(cron = "0/5 * * * * ?")
     public void scheduledCron() {
+        // RestTemplate 객체 생성
+        RestTemplate restTemplate = new RestTemplate();
+        
+        // GET 요청 보내기
+        String response = restTemplate.getForObject("https://jsonplaceholder.typicode.com/posts/1", String.class);
+        
+        // 응답 출력
+        System.out.println(response);
         System.out.println("Schedule Test");
     }
 
