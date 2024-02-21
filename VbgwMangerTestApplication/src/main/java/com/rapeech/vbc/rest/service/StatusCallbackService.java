ec3a423df47848ece7d83c60499299032326a4e9
RapeechDawoon <itdawoonkim@gmail.com>
2024-02-21 16:02:28 +0900

    Change method name

--- VbgwMangerTestApplication/src/main/java/com/rapeech/vbc/rest/service/StatusCallbackService.java
+++ VbgwMangerTestApplication/src/main/java/com/rapeech/vbc/rest/service/StatusCallbackService.java
@@ -4,5 +4,5 @@ import com.rapeech.vbc.rest.model.StatusCallbackRequest;
 import com.rapeech.vbc.rest.model.StatusCallbackResponse;
 
 public interface StatusCallbackService {
-    public StatusCallbackResponse recvStatusCallback(StatusCallbackRequest request);
+    public StatusCallbackResponse requestAndResponse(StatusCallbackRequest request);
 }
