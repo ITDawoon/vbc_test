8ca619bb3decb968422f2a978449ff4ce7808d5f
RapeechDawoon <itdawoonkim@gmail.com>
2024-02-21 15:59:29 +0900

    Remove unused methods

--- VbgwMangerTestApplication/src/main/java/com/rapeech/vbc/rest/controller/StatusCallbackController.java
+++ VbgwMangerTestApplication/src/main/java/com/rapeech/vbc/rest/controller/StatusCallbackController.java
@@ -6,37 +6,39 @@ import springfox.documentation.spring.web.json.Json;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.rapeech.vbc.ha.daemon.VBCSchedular;
 import com.rapeech.vbc.rest.model.StatusCallbackRequest;
 import com.rapeech.vbc.rest.model.StatusCallbackResponse;
 import com.rapeech.vbc.rest.service.StatusCallbackService;
-
 
 @RestController
 @Api(tags = "Status Callback")
 public class StatusCallbackController {
 
-    //@Autowired
-    //private StatusCallbackService service;
+    private static final Logger logger = LoggerFactory.getLogger(StatusCallbackController.class);
+
+    
 
     @ApiOperation(value = "Send Status Callback", notes = "Send status callback for POST requests")
     @RequestMapping(value = "/status/callback", method = RequestMethod.POST)
-    // public StatusCallbackResponse sendStatusCallback(@RequestBody(required = true) StatusCallbackRequest request) {
-
-    //     StatusCallbackResponse result = new StatusCallbackResponse();
-    //     //result = service.recvStatusCallback(request);
-
-    //     return result;
-    // }
     public StatusCallbackRequest sendStatusCallback(@RequestBody StatusCallbackRequest request) {
+        // Log request
+        logger.info("Received status callback request: {}", request);
 
+        // Process request and return
         return request;
     }
 
